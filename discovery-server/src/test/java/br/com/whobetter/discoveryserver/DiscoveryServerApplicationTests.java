58c2844d3773e2acd8ea76f3ecef657ccc299eee
GabrielRossi01 <rossi17006@gmail.com>
2026-07-15 12:32:57 -0300

    feat: adiciona discovery-server, api-gateway e docker-compose para orquestração do postgresql e rabbitmq

---
+++ discovery-server/src/test/java/br/com/whobetter/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.whobetter.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
